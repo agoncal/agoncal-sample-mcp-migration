e60645eec2d4bed070ee713ba6669ab1a0e8141d
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-04-09 14:23:54 +0200

    Fix test

--- mcp-migration-openrewrite/src/test/java/org/agoncal/sample/mcp/migration/openrewrite/JavaMigrationMCPServerTest.java
+++ mcp-migration-openrewrite/src/test/java/org/agoncal/sample/mcp/migration/openrewrite/JavaMigrationMCPServerTest.java
@@ -42,13 +42,13 @@ public class JavaMigrationMCPServerTest {
     @Test
     public void testExecuteThreadStopUnsupportedRecipe() throws IOException {
         ToolResponse result = openRewriteMCPServer.executeThreadStopUnsupportedRecipe();
-        assertTrue(result.content().getFirst().toString().contains("made 2 changes in the code"));
+        assertTrue(result.content().getFirst().toString().contains("made 1 changes in the code"));
     }
 
     @Test
     public void testRemoveTemporalAnnotationRecipe() throws IOException {
         ToolResponse result = openRewriteMCPServer.executeRemoveTemporalAnnotationRecipe();
-        assertTrue(result.content().getFirst().toString().contains("made 2 changes in the code"));
+        assertTrue(result.content().getFirst().toString().contains("made no change in the code"));
     }
 }
 
